40c8519c37c7108c396344c75dd6d725d860f616
Jorge García <jorge7373.jagg@gmail.com>
2018-04-11 09:56:08 -0500

    Se agrego el proyecto, en donde se realizó el catálogo de clientes (alta,baja) aún no está programado, solo es interfáz

---
+++ src/elsolecito_programa/Main.java
@@ -0,0 +1,26 @@
+/*
+ *  Asignación: ElSolecito_Programa
+    Nombre: Jorge Antonio García Gómez
+    Fecha: 11/04/18
+    Descripción: Creación de los formularios del catálogo clientes, modificado
+    hasta la baja SÓLO EN FORMULARIOS.
+ */
+package elsolecito_programa;
+
+/**
+ *
+ * @author jorgegarcia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Inicialización de formulario de Clientes.
+        FRM_Catalogo Catalogo = new FRM_Catalogo();
+        Catalogo.show();
+    }
+    
+}
