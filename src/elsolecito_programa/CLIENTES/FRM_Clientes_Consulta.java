c03a86d70bd37c244ab34f17d47a79892342825a
Jorge García <jorge7373.jagg@gmail.com>
2018-04-14 11:55:37 -0500

    Se agrego la BD con correcciones.

--- src/elsolecito_programa/CLIENTES/FRM_Clientes_Consulta.java
+++ src/elsolecito_programa/CLIENTES/FRM_Clientes_Consulta.java
@@ -88,6 +88,11 @@ public class FRM_Clientes_Consulta extends javax.swing.JFrame {
         jScrollPane1.setViewportView(Tabla_Deudores);
 
         jButton1.setText("Regresar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -125,6 +130,11 @@ public class FRM_Clientes_Consulta extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
